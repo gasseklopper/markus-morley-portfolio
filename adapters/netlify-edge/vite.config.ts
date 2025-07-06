09438f73530a7bd20f2e6955dadafd6ccac03031
gasseklopper <gasseklopper@users.noreply.github.com>
2025-07-06 23:37:06 +0200

    initial: adjust netlify vite config

--- adapters/netlify-edge/vite.config.ts
+++ adapters/netlify-edge/vite.config.ts
@@ -1,6 +1,7 @@
 import { netlifyEdgeAdapter } from "@builder.io/qwik-city/adapters/netlify-edge/vite";
 import { extendConfig } from "@builder.io/qwik-city/vite";
 import baseConfig from "../../vite.config";
+import {VitePWA} from "vite-plugin-pwa";
 
 export default extendConfig(baseConfig, () => {
   return {
@@ -11,6 +12,36 @@ export default extendConfig(baseConfig, () => {
       },
       outDir: ".netlify/edge-functions/entry.netlify-edge",
     },
-    plugins: [netlifyEdgeAdapter()],
+    plugins: [netlifyEdgeAdapter(), VitePWA({                         // ✅ PWA Plugin hier einfügen
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Markus Morley Portfolio',
+        short_name: 'Portfolio',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#0f172a',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/maskable-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })],
   };
 });

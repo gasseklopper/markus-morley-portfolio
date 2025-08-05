acbdcb42648c776a6da99802645703a69657329d
gasseklopper <gasseklopper@users.noreply.github.com>
2025-08-05 22:07:04 +0200

    fix: use supported window hook

--- src/components/template/footer/index.tsx
+++ src/components/template/footer/index.tsx
@@ -1,7 +1,10 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, $, useOnWindow } from "@builder.io/qwik";
 import footerData from "./data";
 
 export const Footer = component$(() => {
+  useOnWindow("load", $(() => {
+    /* noop */
+  }));
   return (
     <footer>
       {footerData.promo && (

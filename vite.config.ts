8045442e320a878c519517f948343fd8b3857ad0
gasseklopper <gasseklopper@users.noreply.github.com>
2025-09-01 19:28:41 +0200

    feat: add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import pkg from "./package.json";
-
+import tailwindcss from "@tailwindcss/vite";
 type PkgDep = Record<string, string>;
 const { dependencies = {}, devDependencies = {} } = pkg as any as {
   dependencies: PkgDep;
@@ -16,62 +16,71 @@ const { dependencies = {}, devDependencies = {} } = pkg as any as {
   [key: string]: unknown;
 };
 errorOnDuplicatesPkgDeps(devDependencies, dependencies);
-
 /**
  * Note that Vite normally starts from `index.html` but the qwikCity plugin makes start at `src/entry.ssr.tsx` instead.
  */
+
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
-    base: '/', // <- hinzufügen!
-    plugins: [qwikCity(), qwikVite(), VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: null,
-      srcDir: 'src',
-      filename: 'sw.ts',
-      strategies: 'injectManifest',
-      includeAssets: ['favicon.png', 'apple-touch-icon.png','fonts/Barlow-Bold.woff',
-        'fonts/Barlow-Medium.woff',
-        'fonts/Barlow-Light.woff',
-        'fonts/Barlow-SemiBold.woff'],
-      outDir: 'dist',
-      workbox: {
-        navigateFallback: '/404',
-      },
-      manifest: {
-        name: 'Markus Morley Portfolio',
-        short_name: 'Portfolio',
-        start_url: '/',
-        scope: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#0f172a',
-        icons: [
-          {
-            src: 'assets/icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'assets/icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'assets/icons/maskable-icon.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
-        ]
-      }
-    }), tsconfigPaths()],
+    base: "/", // <- hinzufügen!
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      VitePWA({
+        registerType: "autoUpdate",
+        injectRegister: null,
+        srcDir: "src",
+        filename: "sw.ts",
+        strategies: "injectManifest",
+        includeAssets: [
+          "favicon.png",
+          "apple-touch-icon.png",
+          "fonts/Barlow-Bold.woff",
+          "fonts/Barlow-Medium.woff",
+          "fonts/Barlow-Light.woff",
+          "fonts/Barlow-SemiBold.woff",
+        ],
+        outDir: "dist",
+        workbox: {
+          navigateFallback: "/404",
+        },
+        manifest: {
+          name: "Markus Morley Portfolio",
+          short_name: "Portfolio",
+          start_url: "/",
+          scope: "/",
+          display: "standalone",
+          background_color: "#ffffff",
+          theme_color: "#0f172a",
+          icons: [
+            {
+              src: "assets/icons/icon-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "assets/icons/icon-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+            {
+              src: "assets/icons/maskable-icon.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+        },
+      }),
+      tsconfigPaths(),
+      tailwindcss(),
+    ],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
       // Put problematic deps that break bundling here, mostly those with binaries.
       // For example ['better-sqlite3'] if you use that in server functions.
       exclude: [],
     },
-
     /**
      * This is an advanced setting. It improves the bundling of your server code. To use it, make sure you understand when your consumed packages are dependencies or dev dependencies. (otherwise things will break in production)
      */
@@ -88,7 +97,6 @@ export default defineConfig(({ command, mode }): UserConfig => {
     //         external: Object.keys(dependencies),
     //       }
     //     : undefined,
-
     server: {
       headers: {
         // Don't cache the server response in dev mode
@@ -103,9 +111,7 @@ export default defineConfig(({ command, mode }): UserConfig => {
     },
   };
 });
-
 // *** utils ***
-
 /**
  * Function to identify duplicate dependencies and throw an error
  * @param {Object} devDependencies - List of development dependencies
@@ -121,27 +127,22 @@ function errorOnDuplicatesPkgDeps(
   const duplicateDeps = Object.keys(devDependencies).filter(
     (dep) => dependencies[dep],
   );
-
   // include any known qwik packages
   const qwikPkg = Object.keys(dependencies).filter((value) =>
     /qwik/i.test(value),
   );
-
   // any errors for missing "qwik-city-plan"
   // [PLUGIN_ERROR]: Invalid module "@qwik-city-plan" is not a valid package
   msg = `Move qwik packages ${qwikPkg.join(", ")} to devDependencies`;
-
   if (qwikPkg.length > 0) {
     throw new Error(msg);
   }
-
   // Format the error message with the duplicates list.
   // The `join` function is used to represent the elements of the 'duplicateDeps' array as a comma-separated string.
   msg = `
     Warning: The dependency "${duplicateDeps.join(", ")}" is listed in both "devDependencies" and "dependencies".
     Please move the duplicated dependencies to "devDependencies" only and remove it from "dependencies"
   `;
-
   // Throw an error with the constructed message.
   if (duplicateDeps.length > 0) {
     throw new Error(msg);

cbe04f2761fb623934736e136715f37b5de00e36
gasseklopper <gasseklopper@users.noreply.github.com>
2025-09-01 16:16:34 +0200

    feat: adopt grid layout with container queries

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,14 +7,16 @@ export default component$(() => {
   useStyles$(styles);
   return (
     <div class="page">
-      <h1 class="title_barlowBold">Hi 👋 barlowBold</h1>
-      <h2 class="title_barlowMedium">Hi 👋 barlowMedium</h2>
-      <h3 class="title_barlowLight">Hi 👋 barlowLight</h3>
-      <h4 class="title_barlowSemibold">Hi 👋 barlowSemibold</h4>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
+      <div class="row" style="--gap: 2rem;">
+        <h1 class="title_barlowBold">Hi 👋 barlowBold</h1>
+        <h2 class="title_barlowMedium">Hi 👋 barlowMedium</h2>
+        <h3 class="title_barlowLight">Hi 👋 barlowLight</h3>
+        <h4 class="title_barlowSemibold">Hi 👋 barlowSemibold</h4>
+        <div>
+          Can't wait to see what you build with qwik!
+          <br />
+          Happy coding.
+        </div>
       </div>
     </div>
   );
